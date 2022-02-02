1fc503a27ac62c42c10a83bfdc65dd2570bd62d5
ZnoGouDj <znogoud3@gmail.com>
2022-02-02 17:22:40 +0300

    Add Loader, Result Context Provider bug fixed, add Footer

--- src/components/Routez.jsx
+++ src/components/Routez.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, Navigate, useRoutes } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Results from './Results';
 
 const Routez = () => {
