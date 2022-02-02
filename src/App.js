afbec7ac31ef81d7806bf469a7637918b873a7c7
ZnoGouDj <znogoud3@gmail.com>
2022-02-02 14:44:02 +0300

    Add some components, add dependencies, fix the tailwind integration

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
 import './index.css';
 
-import { Navbar } from './components/Navbar';
-import { Footer } from './components/Footer';
-import { Router } from './components/Routes';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Routes from './components/Routes';
 
 const App = () => {
   const [darkTheme, setDarkTheme] = useState(false);
 
   return (
     <div className={darkTheme ? 'dark' : ''}>
-      <div className="bg-gray-100">App</div>
+      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+        <Navbar />
+        <Routes />
+        <Footer />
+      </div>
     </div>
   );
 };
