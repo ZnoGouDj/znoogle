0636a383e4c70a8fcaa8429c271ee917558702f8
ZnoGouDj <znogoud3@gmail.com>
2022-02-02 17:52:13 +0300

    Add image search

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -10,11 +10,16 @@ const Results = () => {
   const location = useLocation();
 
   useEffect(() => {
-    getResults('/search/q=JavaScriptMastery&num=40');
-  }, []);
+    if (searchTerm) {
+      if (location.pathname === '/videos') {
+        getResults(`/search/q=${searchTerm} videos`);
+      } else {
+        getResults(`${location.pathname}/q=${searchTerm}&num=40`);
+      }
+    }
+  }, [searchTerm, location.pathname]);
 
   if (isLoading) return <Loading />;
-  console.log(location.pathname);
 
   switch (location.pathname) {
     case '/search':
@@ -31,7 +36,16 @@ const Results = () => {
         </div>
       );
     case '/images':
-      return 'IMAGES';
+      return (
+        <div className="flex flex-wrap justify-center items-center">
+          {results?.image_results?.map(({ image, link: { href, title } }, index) => (
+            <a className="sm:p-3 p-5" href={href} key={index} target="_blank" rel="noreferrer">
+              <img src={image?.src} alt={title} loading="lazy" />
+              <p className="w-36 break-words text-sm mt-2">{title}</p>
+            </a>
+          ))}
+        </div>
+      );
     case '/news':
       return 'NEWS';
     case '/videos':
