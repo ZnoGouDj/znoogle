0ccd436a5dfcf369bc3c057e1c826b7be07abe2f
ZnoGouDj <znogoud3@gmail.com>
2022-02-03 12:27:53 +0300

    Add search functionality

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -68,7 +68,9 @@ const Results = () => {
         <div className="flex flex-wrap">
           {results.map((video, index) => (
             <div key={index} className="p-2">
-              <ReactPlayer url={video.additional_links?.[0].href} controls width="355px" height="200px" />
+              {video?.additional_links?.[0]?.href.includes('watch') && (
+                <ReactPlayer url={video.additional_links?.[0].href} controls width="355px" height="200px" />
+              )}
             </div>
           ))}
         </div>
